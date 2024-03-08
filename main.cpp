f914c2f2b577c5ecab6daacb4534165d4c8fa037
catz <133279982+catzSpace@users.noreply.github.com>
2024-03-07 22:14:05 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "./src/window.hpp"
+
+using namespace std;
+
+int main()
+{
+  window();
+  return 0;
+}
+
+
+/*
+nombre del proyecto: Pong!
+autor: CatzSpace
+Desc: clasico juego de pong con c++
+hecho con fines de practica.
+*/
